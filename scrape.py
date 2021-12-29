0e328dbd7767d22e97f8f85e4374fbc31e6fef81
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2021-12-29 09:10:52 +0100

    Working solution of cloud upload

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import os
 import requests
 import tempfile
 
-from boto3 import resource
+from boto3 import client
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
@@ -20,7 +20,7 @@ class Scraper():
             "User-agent": os.getenv("USER-AGENT")  # noqa: E501
         }
 
-        self.s3 = resource(
+        self.s3 = client(
             service_name="s3",
             region_name="eu-north-1",
             aws_access_key_id=os.getenv("AWS-KEY-ID"),
@@ -31,7 +31,7 @@ class Scraper():
 
         self.path = os.path.join(os.getcwd(), folder)
 
-    def get_images(self):
+    def get_images(self, image_listing_tag: str = "products-listing small"):
         """Scrape images."""
         try:
             os.mkdir(self.path)
@@ -39,40 +39,49 @@ class Scraper():
             pass
         # Fetch image tags
         r = requests.get(self.url, headers=self.headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        garments = soup.find("ul", attrs={"class": "products-listing small"})
+        if r.status_code != 200:
+            raise Exception("Request not succesful.")
+        garments = BeautifulSoup(r.text, 'html.parser').find(
+            "ul",
+            attrs={"class": image_listing_tag}
+        )
         self.image_tags = garments.find_all("img")
 
     def write_images(self):
         """Write images to disk."""
         for image_tag in self.image_tags:
+            
             name = image_tag['alt']
             link = image_tag['data-src']
-            with open(self.path + "/" + name.replace(' ', '-').replace('/', '') + '.jpg', 'wb') as f:  # noqa: E501
+            fn = self.path + "/" + name.replace(" ", "-").replace("/", "") + ".jpg" # noqa 3501
+
+            with open(fn, "wb") as f:
                 im = requests.get("https:" + link, headers=self.headers)
                 f.write(im.content)
-                print('Writing: ', name)
+                print("Writing: ", name)
 
     def upload_images(self):
         """Push images to cloud."""
-        # Save each image
         for image_tag in self.image_tags:
+
             name = image_tag['alt']
             link = image_tag['data-src']
             image = requests.get("https:" + link, headers=self.headers).content
 
-            with tempfile.NamedTemporaryFile() as tmp:
-                tmp.write(image)
-                self.s3.Bucket('tfc-garments').upload_file(
-                    Filename=tmp.name,
-                    Key=name + ".jpg"
+            with tempfile.NamedTemporaryFile() as tf:
+                tf.write(image)
+                tf.seek(0)
+                self.s3.upload_file(
+                    tf.name,
+                    Bucket="tfc-garments",
+                    Key=name
                 )
 
 
 class HMScraper(Scraper):
     """H&M Scraper."""
 
-    def __init__(self, url, folder, ):
+    def __init__(self, url: str, folder: str):
         super(HMScraper, self).__init__(url, folder)
 
     def get_images(self):
@@ -82,7 +91,7 @@ class HMScraper(Scraper):
 class ZalandoScraper(Scraper):
     """Zalando Scraper"""
 
-    def __init__(self, url, folder):
+    def __init__(self, url: str, folder: str):
         super(ZalandoScraper, self).__init__(url, folder)
 
     def get_images(self):
@@ -92,10 +101,8 @@ class ZalandoScraper(Scraper):
 if __name__ == "__main__":
     # Scrape from H&M
     sc = HMScraper(
-        url="https://www2.hm.com/sv_se/herr/produkter/se-alla.html?sort=stock&image-size=small&image=stillLife&offset=0&page-size=52",  # noqa: E501
+        url="https://www2.hm.com/sv_se/herr/produkter/se-alla.html?sort=stock&image-size=small&image=stillLife&offset=0&page-size=100",  # noqa: E501
         folder="hm"
     )
     sc.get_images()
     sc.upload_images()
-
-load_dotenv()
