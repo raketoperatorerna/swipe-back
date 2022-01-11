53f00280e510481e4bfc5666d36146c86d1b392c
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2022-01-11 08:31:13 +0100

    Get picture

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { S3 } from "@aws-sdk/client-s3";
+
 import dotenv from 'dotenv';
 import express from "express";
 import mongoose from 'mongoose';
@@ -15,11 +17,15 @@ dotenv.config();
 
 // Connect to mongodb
 mongoose.connect(process.env.dbURL)
-    .then(() => {console.log('connected'); app.listen(process.env.PORT);})
+	.then(() => {console.log('connected'); app.listen(process.env.PORT);})
 	.catch((err) => console.log(err));
 
 // Connect to s3
-
+const s3 = new S3({
+    region: 'eu-north-1',
+    accessKeyId: process.env.AWS_KEY_ID,
+    secretAccessKey: process.env.AWS_KEY_PASS,
+}) 
 
 app.post('/event', (req, res) => {
     const s = swipe.Swipe({
@@ -28,7 +34,6 @@ app.post('/event', (req, res) => {
 	action: req.body.action
     })
     s.save().then((result) => {
-	console.log(result)
 	res.send(result)
     }).catch((err) => console.log(err))
 })
@@ -42,11 +47,6 @@ async function getClothing() {
     return products[parseInt(Math.random() * products.length)];
 }
 
-app.get("/gets3", async function (req, res) {
-    const clothing = await getClothing();
-    res.send(clothing);
-});
-
 app.get("/get", async function (req, res) {
     const clothing = await getClothing();
     res.send(clothing);
@@ -56,3 +56,18 @@ app.get("/getdatabase", async function (req, res) {
     await db.read();
     res.send(db.data);
 });
+
+app.get("/getimage", function(req, res, next) {
+    var params = { Bucket: "tfc-garments", Key: "10-pack mid trunks i bomull" };
+
+    s3.getObject(params, function(err, data) {
+	// Handle any error and exit
+	if (err)
+            return err;
+	// No error happened
+	// Convert Body from a Buffer to a String
+	console.log(data.Body)
+	let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
+    });
+});
+
